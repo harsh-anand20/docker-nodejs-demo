9bc7076a5758590554d37729dd2a830b6397131e
Harsh <harsh.anand20999@gmail.com>
2022-06-29 23:35:40 +0530

    Added index.html file

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
-    res.send("Hello World!");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function(){
